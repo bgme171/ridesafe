8bedf1cad72a317d8161b39f5fad4c4674d78bc9
Berni Mora <bgme171@gmail.com>
2021-12-05 13:52:38 -0600

    Make AlarmActivity fullscreen

--- app/src/main/java/com/example/ridesafe/AlarmActivity.java
+++ app/src/main/java/com/example/ridesafe/AlarmActivity.java
@@ -11,6 +11,7 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.provider.ContactsContract;
@@ -124,6 +125,18 @@ public class AlarmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
+        View decorView = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+
 
         tv_alarm_timer = findViewById(R.id.tv_alarm_timer);
         tv_alarm_timer.setTextSize(50);
